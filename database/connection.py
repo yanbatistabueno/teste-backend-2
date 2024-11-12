179b20dbcad96592a9252682e1291c0969436cf5
Yan Batista <yanbatista2704@gmail.com>
2024-11-12 01:00:55 -0300

    setup do projeto

--- database/connection.py
+++ database/connection.py
@@ -1,18 +1,11 @@
 import mysql.connector
-# connection = mysql.connector.connect(
-#     host='192.168.1.12',
-#     user='myuser',
-#     password='123456',
-#     database='rick_and_morty'
-# )
-
 connection = mysql.connector.connect(
-    host='127.0.0.1',
-    user='Yan',
-    password='Gohanyan1',
-    database='rick_and_morty',
-    use_unicode=True
+    host='192.168.1.12',
+    user='myuser',
+    password='123456',
+    database='rick_and_morty'
 )
+
 
 cursor = connection.cursor(buffered=True)
 cursor.execute("USE rick_and_morty") # select the database
